28d3bd5bfac16900c1aee6c3f3d8cdff8825eb00
Esha Susan Shaji <eshasusanshaji@gmail.com>
2025-12-27 19:52:52 +0530

    Add App.jsx and styling

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ResumeAnalyzer from './components/ResumeAnalyzer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ResumeAnalyzer />
+    </div>
+  );
+}
+
+export default App;
